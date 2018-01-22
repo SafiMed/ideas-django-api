9fe9dd192f87eb824801be2f9d8d842b53e595ff
Mohammed Safi <safi.mohammed59@gmail.com>
2018-01-22 03:16:24 +0000

    Added permissions and authentication to our UserViewSet.

--- ideas_api/views.py
+++ ideas_api/views.py
@@ -4,9 +4,11 @@ from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
 
 from . import models
 from . import serializers
+from . import permissions
 
 # Create your views here.
 
@@ -16,4 +18,5 @@ class UserViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserSerializer
     queryset = models.User.objects.all()
-    
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnUser,)
