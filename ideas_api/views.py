a915a72c752210e7e7c706989273fb9fc41ca511
Mohammed Safi <safi.mohammed59@gmail.com>
2018-01-22 03:03:21 +0000

    Added user listing and creating feature.

--- ideas_api/views.py
+++ ideas_api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from . import models
+from . import serializers
+
 # Create your views here.
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating users."""
+
+    serializer_class = serializers.UserSerializer
+    queryset = models.User.objects.all()
+    
