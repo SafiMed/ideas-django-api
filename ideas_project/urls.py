9b94e868370e19ba305c038a068915042ea2f852
Mohammed Safi <safi.mohammed59@gmail.com>
2018-01-27 23:18:17 +0000

    Added Idea API test.

--- ideas_project/urls.py
+++ ideas_project/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('ideas_api.urls'))
+    url(r'^api/', include('ideas_api.urls', namespace='api'))
 ]
